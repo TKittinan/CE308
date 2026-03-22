8aee50f389c021d2da0868d8d64d0c09b455258a
tee <tkittinan.work@gmail.com>
2026-03-22 14:37:47 +0700

    Update

--- WorkShop10/app/index.tsx
+++ WorkShop10/app/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import TodoScreen from './screens/TodoScreen';
+import CartScreen from './screens/CartScreen'
+// import TodoScreen from './screens/TodoScreen'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <TodoScreen />
+      <CartScreen />
+      {/* <TodoScreen/> */}
     </Provider>
   );
 }
